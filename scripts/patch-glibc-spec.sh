14d91756eb7aed8b137483c232e55fd3986fdfb6
asmx2 <asmx2@noreply.codeberg.org>
2026-08-08 00:08:42 +0300

    Build fix

--- scripts/patch-glibc-spec.sh
+++ scripts/patch-glibc-spec.sh
@@ -20,15 +20,15 @@ if grep -qE -- '-march=x86-64(-v[0-9])?' "$SPEC"; then
     sed -i -E 's/-march=x86-64(-v[0-9])?/-march=znver4/g' "$SPEC"
 fi
 
-sed -i "/^%build$/a\
-export CFLAGS=\"\${CFLAGS:+\$CFLAGS }-march=znver4 -mtune=znver4\"\
-export CXXFLAGS=\"\${CXXFLAGS:+\$CXXFLAGS }-march=znver4 -mtune=znver4\"\
-export RPM_OPT_FLAGS=\"\${RPM_OPT_FLAGS:+\$RPM_OPT_FLAGS }-march=znver4 -mtune=znver4\"" "$SPEC"
+sed -i '/^%build$/a \
+export CFLAGS="${CFLAGS:+$CFLAGS }-march=znver4 -mtune=znver4"\
+export CXXFLAGS="${CXXFLAGS:+$CXXFLAGS }-march=znver4 -mtune=znver4"\
+export RPM_OPT_FLAGS="${RPM_OPT_FLAGS:+$RPM_OPT_FLAGS }-march=znver4 -mtune=znver4"\
+sed -i '\''s|\\$(common-objpfx)elf/ld.so|\\$(test-wrapper) \\$(common-objpfx)elf/ld.so|g'\'' localedata/Makefile' "$SPEC"
 
 sed -i "1i %global __make /usr/bin/make test-wrapper=\"$SDE64 -spr --\"" "$SPEC"
 
 sed -i -E "s@(\\\$RPM_BUILD_ROOT|%\{buildroot\})%\{_bindir\}/localedef@$SDE64 -spr -- &@g" "$SPEC"
-
 sed -i -E "s@(\\\$RPM_BUILD_ROOT|%\{buildroot\})%\{_sbindir\}/iconvconfig@$SDE64 -spr -- &@g" "$SPEC"
 
 for var_pattern in 'BuildFlags=' 'build_CFLAGS=' 'glibc_flags_cflags='; do
